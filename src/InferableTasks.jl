8f70125205160eb7243e4313682fe31ab0517d15
Takafumi Arakaki <aka.tkf@gmail.com>
2020-08-25 17:36:23 -0700

    Initial implementation

--- src/InferableTasks.jl
+++ src/InferableTasks.jl
@@ -1,5 +1,35 @@
 module InferableTasks
 
-# Write your package code here.
+export @iasync, @ispawn
+
+struct InferableTask{T}
+    task::Task
+end
+
+Base.fetch(t::InferableTask{T}) where {T} = fetch(t.task)::T
+Base.wait(t::InferableTask) = wait(t.task)
+
+macro ispawn(ex)
+    inferrable(ex) do ex
+        :($Threads.@spawn $ex)
+    end |> esc
+end
+
+macro iasync(ex)
+    inferrable(ex) do ex
+        :($Base.@async $ex)
+    end |> esc
+end
+
+function inferrable(spawn_macro, ex)
+    @gensym f T world
+    quote
+        local $f, $T, $world
+        $f() = $ex
+        $T = $Core.Compiler.return_type($f, $Tuple{})
+        $world = $Base.get_world_counter()
+        $InferableTask{$T}($(spawn_macro(:($Base.invoke_in_world($world, $f)))))
+    end
+end
 
 end
