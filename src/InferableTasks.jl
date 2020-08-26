246e6efbbdfc953db0d173ca03fb5c043f254c8c
Takafumi Arakaki <aka.tkf@gmail.com>
2020-08-26 15:12:09 -0700

    Trying to workaround missing back edges

--- src/InferableTasks.jl
+++ src/InferableTasks.jl
@@ -21,11 +21,22 @@ macro iasync(ex)
     end |> esc
 end
 
+# I'm hoping this would make sure that the caller of `@ispawn` would
+# be invalidated if the function `$f` has to be invalidated.  If the
+# caller is invalidated, it'll get a new world age.  This in turn,
+# invalidates `$f` called in the task because it is now called from a
+# new world age (via `invoke_in_world`).
+# --- https://discourse.julialang.org/t/can-we-have-inferable-fetch-task/45541/4
+const __NEVER = Ref(false)
+
 function inferable(spawn_macro, ex)
     @gensym f T world
     quote
         local $f, $T, $world
-        $f() = $ex
+        $Base.@noinline $f() = $ex
+        if $__NEVER[]
+            $f()
+        end
         $T = $Core.Compiler.return_type($f, $Tuple{})
         $world = $Base.get_world_counter()
         $InferableTask{$T}($(spawn_macro(:($Base.invoke_in_world($world, $f)))))
